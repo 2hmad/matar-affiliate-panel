e966a9e38a36eedd360ea2d6d4a40888caba8f57
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2022-06-09 20:21:50 +0200

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('login', [AuthController::class, 'loginAccount']);
 Route::post('register', [AuthController::class, 'createAccount']);
 
