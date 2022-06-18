51fcdea5752ad1f01f79cf4ca55a3c9d944205b6
Ahmed Mohamed Ibrahim <32125808+Ahmed-Ibrahimm@users.noreply.github.com>
2022-06-18 02:55:09 +0200

    update

--- resources/js/src/router/index.js
+++ resources/js/src/router/index.js
@@ -168,7 +168,7 @@ router.beforeEach((to, from, next) => {
             next();
             return;
         }
-        location.href = "/login";
+        next("/login");
     } else {
         next();
     }
@@ -177,7 +177,7 @@ router.beforeEach((to, from, next) => {
 router.beforeEach((to, from, next) => {
     if (to.matched.some((record) => record.meta.guest)) {
         if (store.getters.isAuthenticated) {
-            location.href = "/";
+            next("/");
             return;
         }
         next();
